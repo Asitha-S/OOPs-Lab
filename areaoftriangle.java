1e0a28a24c7e5113e46409a61dc31a95bbbe0378
Asitha-S <asisrithar@gmail.com>
2024-12-31 15:09:32 +0530

    Add files via upload

---
+++ areaoftriangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class areaoftriangle {
+	public static void main(String[] args) {
+		int a,b;
+		System.out.println("Enter the length of one side");
+		Scanner length= new Scanner(System.in);
+		a=length.nextInt();
+		System.out.println("Enter the breadth");
+		Scanner breadth= new Scanner(System.in);
+		b=breadth.nextInt();
+		int c=(a*b)/2;
+		System.out.println("The area of the triangle is " + c);
+}
+}
