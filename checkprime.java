71efdb908c4ebc0b9943b5ec2440495f628be605
Asitha-S <asisrithar@gmail.com>
2025-01-07 14:39:15 +0530

    Add files via upload

---
+++ checkprime.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class checkprime{
+	public static void main(String[] args){
+		int a,n,i;
+		System.out.println("Enter a number:");
+		Scanner num=new Scanner(System.in);
+		a=num.nextInt();
+		n=0;
+		for (i=2;i<a;i++){
+			if (a%i==0){
+				n++;}}
+		if (n==0){
+			System.out.println(a+" is a prime number");}
+		else{
+			System.out.println(a+" is not a prime number");}
+}}
