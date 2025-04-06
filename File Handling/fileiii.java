816bb4111bcc298a611c8feba66f0f237117ef03
Asitha-S <asisrithar@gmail.com>
2025-04-06 21:16:16 +0530

    Create fileiii.java

---
+++ File Handling/fileiii.java
@@ -0,0 +1,16 @@
+import java.io.File; 
+import java.io.IOException;   
+public class fileiii { 
+    public static void main(String[] args) { 
+        try { 
+            File file = new File("newfile.txt"); 
+            if (file.createNewFile()) { 
+                System.out.println("File created: " + file.getName()); 
+            } else { 
+                System.out.println("File already exists."); 
+            } 
+        } catch (IOException e) { 
+            System.out.println("An error occurred while creating the file."); 
+        } 
+    } 
+} 
