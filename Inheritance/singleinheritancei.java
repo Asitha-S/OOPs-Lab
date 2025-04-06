f1d12c11f5501ff00cd3fe45499994e660f71a08
Asitha-S <asisrithar@gmail.com>
2025-04-06 18:00:29 +0530

    Add files via upload

---
+++ Inheritance/singleinheritancei.java
@@ -0,0 +1,21 @@
+class Animal { 
+    void sound() { 
+        System.out.println("Animals make sound"); 
+
+ 
+    } 
+} 
+  
+class Dog extends Animal { 
+    void bark() { 
+        System.out.println("Dog barks"); 
+    } 
+} 
+  
+public class singleinheritancei { 
+    public static void main(String[] args) { 
+        Dog d = new Dog(); 
+        d.sound(); 
+        d.bark(); 
+    }
+}
