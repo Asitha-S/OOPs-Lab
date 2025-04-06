aa2cf657ce4c858fabdb7a1f265870939be30570
Asitha-S <asisrithar@gmail.com>
2025-04-06 19:07:31 +0530

    Create methodoverridingi.java

---
+++ Polymorphism/methodoverridingi.java
@@ -0,0 +1,20 @@
+class Animal { 
+    void sound() { 
+        System.out.println("Animal makes a sound"); 
+    } 
+} 
+  
+class Dog extends Animal { 
+
+    @Override 
+    void sound() { 
+        System.out.println("Dog barks"); 
+    } 
+} 
+  
+public class methodoverridingi { 
+    public static void main(String[] args) { 
+        Animal myDog = new Dog(); 
+        myDog.sound();   
+    } 
+}
