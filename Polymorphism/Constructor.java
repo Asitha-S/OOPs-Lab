1b26dd4b66aa6d39abf0f6be424bb02b3d3cf143
Asitha-S <asisrithar@gmail.com>
2025-04-06 19:02:42 +0530

    Create Constructor.java

---
+++ Polymorphism/Constructor.java
@@ -0,0 +1,8 @@
+public class Constructor { 
+  int x; 
+   public Main() { 
+    x = 5; } 
+  public static void main(String[] args) { 
+    Main myObj = new Main(); 
+    System.out.println(myObj.x); 
+  }}
